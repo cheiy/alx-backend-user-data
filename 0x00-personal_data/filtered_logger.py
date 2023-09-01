18cf28b9af8f36094843b361808e72d851dd1ed5
cheiy <gschege@gmail.com>
2023-09-01 00:39:33 +0000

    Filtered logger

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Module contains filter_datum function
+"""
+from typing import List
+import re
+
+patterns = {
+        'extract': lambda x, y: r'(?P<field>{})=[^{}]*'.format('|'.join(x), y),
+        'replace': lambda x: r'\g<field>={}'.format(x),
+        }
+
+
+def filter_datum(fields: List, redaction: str,
+                 message: str, separator: str) -> str:
+    """
+    Given a log message, function returns the obfuscated version of the same
+    """
+    extract, replace = (patterns["extract"], patterns["replace"])
+    return re.sub(extract(fields, separator), replace(redaction), message)
