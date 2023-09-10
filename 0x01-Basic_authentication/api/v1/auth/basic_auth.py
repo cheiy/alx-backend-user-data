72e0ec427744a62acc234eae3a4141eefd885a50
cheiy <gschege@gmail.com>
2023-09-10 15:30:24 +0000

    Basic -Base64

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,4 +9,13 @@ class BasicAuth(Auth):
     """
     Class inherits from Auth to perform authentication
     """
-    pass
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if authorization_header.split(" ")[0] != "Basic":
+            return None
+        else:
+            return authorization_header.split(" ")[1]
