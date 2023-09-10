69e027863d0a10b16b39f01d1b937a15e1e1459a
cheiy <gschege@gmail.com>
2023-09-10 19:42:54 +0000

    Basic - Overload current_user - and BOOM!

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -78,3 +78,15 @@ class BasicAuth(Auth):
             if user.is_valid_password(user_pwd):
                 return user
         return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Method gets the current user"""
+        try:
+            auth_header = self.authorization_header(request)
+            enc = self.extract_base64_authorization_header(auth_header)
+            dec = self.decode_base64_authorization_header(enc)
+            email, pwd = self.extract_user_credentials(dec)
+            return self.user_object_from_credentials(email, pwd)
+        except Exception:
+            return None
